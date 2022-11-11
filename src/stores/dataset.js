4a4441470a616d5fcd3bd9a7ea7d4c561a5d7f63
Loïc Monard <monard@intechinfo.fr>
2022-11-11 16:00:01 +0100

    :sparkles: Setting up the HomeView view with the dataset store

--- src/stores/dataset.js
+++ src/stores/dataset.js
@@ -8,6 +8,10 @@ export const useDatasetStore = defineStore("dataset", () => {
    */
   const datasets = ref(Dataset);
   /**
+   * The active category
+   */
+  const activeCategory = ref(null);
+  /**
    * Computed property that returns a dictionnary of categories grouped by dataset depending on
    * category_id
    *
@@ -22,10 +26,46 @@ export const useDatasetStore = defineStore("dataset", () => {
     });
     return categories;
   });
+  /**
+   * Computed to return the collections of the active category
+   */
+  const activeCategoryCollections = computed(() => {
+    return datasetFilteredByCategoryId.value[activeCategory.value];
+  });
+  /**
+   * Method to set the active category
+   * @param {*} category_id
+   */
+  const setActiveCategory = (category_id) => {
+    activeCategory.value = category_id;
+  };
+  /**
+   * Method to return the total number of "count?" of a category
+   * @param {*} category_id
+   * @returns {String}
+   */
+  const getDocCountPerCategory = (category_id) => {
+    return datasetFilteredByCategoryId.value[category_id]
+      .reduce((acc, dataset) => acc + (dataset._source?.count || 0), 0)
+      .toLocaleString();
+  };
+  /**
+   * Method to filter the dataset by a search term
+   */
+  const getCollectionsBySearch = computed(() => {
+    return (value) => {
+      return datasets.value.filter((dataset) =>
+        dataset.label.toLowerCase().includes(value.toLowerCase())
+      );
+    };
+  });
 
-  const datasetNames = computed(() =>
-    datasets.value.map((dataset) => dataset.name)
-  );
-
-  return { datasets, datasetFilteredByCategoryId, datasetNames };
+  return {
+    datasets,
+    datasetFilteredByCategoryId,
+    activeCategoryCollections,
+    setActiveCategory,
+    getDocCountPerCategory,
+    getCollectionsBySearch,
+  };
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,96 @@
-<script>
+<script setup>
 import { storeToRefs } from "pinia";
-import { defineComponent } from "vue";
 import { useDatasetStore } from "../stores/dataset";
+import Category from "../components/Category.vue";
+import Collection from "../components/Collection.vue";
+import { ref, Transition } from "vue";
+import { computed } from "@vue/reactivity";
+import { useRouter } from "vue-router";
 
-export default defineComponent({
-  setup() {
-    const datasetStore = useDatasetStore();
-    const { datasets, datasetFilteredByCategoryId, datasetNames } =
-      storeToRefs(datasetStore);
+const router = useRouter();
 
-    return {
-      datasets,
-      datasetFilteredByCategoryId,
-      datasetNames,
-    };
-  },
-});
+const goToCategory = (category) => {
+  router.push("/categories/" + category);
+};
+
+const search = ref("");
+
+const isSearchEmpty = computed(() => search.value.length === 0);
+
+const datasetStore = useDatasetStore();
+const { datasetFilteredByCategoryId } = storeToRefs(datasetStore);
 </script>
 
 <template>
-  <main>
-    This is the default view. {{ datasets.length }},
-    {{ datasetFilteredByCategoryId }}
+  <main
+    class="container mx-auto h-full w-fit flex flex-col justify-center items-center gap-4 relative"
+  >
+    <div class="h-screen flex flex-col justify-center items-center gap-4">
+      <input
+        v-model="search"
+        type="text"
+        placeholder="Browse collections"
+        class="max-w-[420px] mt-[30vh] bg-white py-2 px-4 rounded-lg border w-full"
+      />
+      <Transition name="fade" mode="out-in">
+        <div
+          v-if="isSearchEmpty"
+          class="flex-1 w-fit grid gap-4 grid-cols-2 auto-rows-max"
+        >
+          <Category
+            v-for="(value, key) in datasetFilteredByCategoryId"
+            @click="goToCategory(key)"
+            :key="key"
+            :title="key"
+            :documents="value"
+          />
+        </div>
+        <div
+          v-else="isSearchEmpty"
+          class="h-full h-max-[100vh] grid gap-4 grid-cols-2 overflow-auto auto-rows-max p-2"
+        >
+          <Collection
+            v-for="(collection, index) in datasetStore.getCollectionsBySearch(
+              search
+            )"
+            :key="index"
+            :collection="collection"
+          />
+          <span v-if="datasetStore.getCollectionsBySearch(search).length === 0">
+            No results found
+          </span>
+        </div>
+      </Transition>
+    </div>
+    <div
+      class="fixed bottom-0 w-full h-[150px] bg-gradient-to-t from-slate-50 to-transparent pointer-events-none"
+    ></div>
   </main>
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.15s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  transform: translateY(20px);
+  opacity: 0;
+}
+::-webkit-scrollbar {
+  width: 6px;
+}
+::-webkit-scrollbar-thumb {
+  background-color: rgb(227, 227, 227);
+  border-radius: 10px;
+  transition: all var(--transition-duration) ease;
+}
+::-webkit-scrollbar-thumb:hover {
+  background-color: rgb(200, 200, 200);
+}
+::-webkit-scrollbar-track {
+  border-radius: 10px;
+}
+</style>
