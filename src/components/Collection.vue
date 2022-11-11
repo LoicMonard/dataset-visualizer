a3865ab18ebc3c8ed1b6a128b03674cfb2076ea2
Loïc Monard <monard@intechinfo.fr>
2022-11-11 15:59:12 +0100

    :sparkles: Create the Collection document

---
+++ src/components/Collection.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  props: {
+    collection: {
+      type: Object,
+      required: true,
+    },
+  },
+  setup() {
+    const imgId = Math.floor(Math.random() * 200) + 1;
+
+    return {
+      imgId,
+    };
+  },
+};
+</script>
+
+<template>
+  <div
+    class="w-[200px] h-fit w-fit min-w-[150px] flex flex-wrap rounded-lg cursor-pointer shadow-md hover:shadow-lg hover:scale-105 transition-all duration-150 overflow-hidden"
+  >
+    <div class="w-full">
+      <div class="bg-white p-4 w-full flex flex-col gap-4">
+        <h3 class="text-l truncate font-medium capitalize">
+          📖 {{ collection.label }}
+        </h3>
+        <p class="text-xs text-gray-600 line-clamp-3">
+          {{ collection.summary }}
+        </p>
+        <div class="flex flex-1 justify-between">
+          <p class="text-xs">📂{{ collection._source?.category_id }}</p>
+          <a
+            target="_blank"
+            :href="collection._source?.data_url || collection._source?.info_url"
+            class="text-xs text-blue-400"
+          >
+            LINK
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
